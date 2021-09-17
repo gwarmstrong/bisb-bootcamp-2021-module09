fe5e87663647fe066b06ed1d36f914aceea43bf3
George Armstrong <garmstrong@colgate.edu>
2021-09-17 09:07:32 -0700

    update student files

---
+++ bootcamp_module08/core/student_95.py
@@ -0,0 +1,30 @@
+def count_substring(string, substring):
+    """Counts the number of occurrences of `substring` in `string`
+
+    Parameters
+    ----------
+    string : str
+        The string to count within
+    substring : str
+        The value to count in string
+
+    Returns
+    -------
+    int
+        The number of times `substring` occurs in `string`
+
+    """
+    count = 0
+
+    string_length = len(string)
+    substring_length = len(substring)
+    n_subsequences = string_length - substring_length + 1
+
+    for i in range(n_subsequences):
+        left_bound = i
+        right_bound = i + substring_length
+        candidate_substring = string[left_bound:right_bound]
+        if candidate_substring == substring:
+            count += 1
+
+    return count
